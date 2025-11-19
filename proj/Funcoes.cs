e489dd4a803d37aedd1b1667b3439dcd5eeacdf2
Dig-San16 <digsantos.hoode@gmail.com>
2025-11-19 17:18:40 -0300

    digas

--- proj/Funcoes.cs
+++ proj/Funcoes.cs
@@ -10,10 +10,11 @@ namespace proj
 {
     class Funcoes
     {
-        static string conexaoString = "server=localhost;uid=root;pwd=Rnt915302@;database=nutricao;port=3306";
+        static string conexaoString = "server=localhost;uid=root;pwd=;database=nutricao;port=3306";
         public static void CadastrarUsuario()
         {
-            while (true){
+            while (true)
+            { 
                 try {
 
                     Console.WriteLine("CADASTRO DE USUARIO");
