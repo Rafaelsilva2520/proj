410c52cadbe3801117b456dde6ceb9e8fdf5e2a5
Rafaelsilva2520 <rafael.silva2520@etec.sp.gov.br>
2025-11-25 21:20:41 -0300

    Update connection string and enhance error handling
    Updated connection string to use 'root' password and improved error messages in user registration and diet registration methods.

--- proj/Funcoes.cs
+++ proj/Funcoes.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,12 +10,14 @@ namespace proj
 {
     class Funcoes
     {
-        static string conexaoString = "server=localhost;uid=root;pwd=;database=nutricao;port=3306";
+        static string conexaoString = "server=localhost;uid=root;pwd=root;database=nutricao;port=3306";
+
         public static void CadastrarUsuario()
         {
-            while (true){
-                try {
-
+            while (true)
+            { 
+                try
+                {
                     Console.WriteLine("CADASTRO DE USUARIO");
 
                     Console.Write("NOME: ");
@@ -38,7 +40,6 @@ namespace proj
                         conexao.Open();
                         Console.WriteLine($"BOA MLK ! USUARIO {Pessoa.nome} CADASTRADO COM SUCESSO!");
 
-
                         using (MySqlCommand cmd = new MySqlCommand(Database.sqlInsertPac, conexao))
                         {
                             cmd.Parameters.AddWithValue("@nome", Pessoa.nome);
@@ -46,19 +47,23 @@ namespace proj
                             cmd.Parameters.AddWithValue("@cpf", Pessoa.cpf);
                             cmd.Parameters.AddWithValue("@peso", Pessoa.peso);
                             cmd.Parameters.AddWithValue("@altura", Pessoa.altura);
-
                             cmd.ExecuteNonQuery();
-                            break;
                         }
-                    } 
-
-                } catch (Exception e)
+                    }
+                    //aqui acaba uma funçao
+                    break;
+                }
+                catch (Exception e)
                 {
-                    Console.WriteLine($"Opção inválida, tente novamente {e}");
-                    continue;
+                    Console.WriteLine($"Opção inválida, tente novamente. Erro: {e.Message}");
                 }
             }
-        }   
+        }
+        //msg de opç inválida
+
+
+
+
         public static void CadastrarDieta()
         {
             while (true)
@@ -66,27 +71,56 @@ namespace proj
                 try
                 {
                     Console.WriteLine("CADASTRO DE DIETA");
+                    Console.WriteLine("OLA " + Pessoa.nome + " vamos cadastrar sua dieta!");
+                    Console.WriteLine("-------------------------------");
+                    Console.WriteLine("Informe os dados abaixo:");
                     Console.Write("CPF do paciente: ");
                     long cpf = long.Parse(Console.ReadLine());
 
-                    Console.WriteLine("\nOBJETIVO: \n1 - Emagrecer \n2 - Ficar forte \n3 - Saúde");
-                    Console.Write("Opção: ");
-                    Dieta.Objetivo = int.Parse(Console.ReadLine());
+                    Console.Write("Carboidratos consumidos por dia (g): ");
+                    Dieta.carbo = int.Parse(Console.ReadLine());
 
-                    if (Dieta.Objetivo < 1 || Dieta.Objetivo > 3)
+                    Console.Write("Proteínas consumidas por dia (g): ");
+                    Dieta.prot = int.Parse(Console.ReadLine());
+
+                    Console.Write("Lipídios (gorduras) consumidos por dia (g): ");
+                    Dieta.lip = int.Parse(Console.ReadLine());
+
+                    // Cálculo das calorias
+                    Dieta.calorias = (Dieta.carbo * 4) + (Dieta.prot * 4) + (Dieta.lip * 9);
+
+                    Console.WriteLine($"\nCALORIAS TOTAIS CONSUMIDAS: {Dieta.calorias} kcal");
+
+                    string nutrienteMaisConsumido;
+
+                    if (Dieta.carbo > Dieta.prot && Dieta.carbo > Dieta.lip)
+                        nutrienteMaisConsumido = "Carboidratos";
+                    else if (Dieta.prot > Dieta.carbo && Dieta.prot > Dieta.lip)
+                        nutrienteMaisConsumido = "Proteínas";
+                    else if (Dieta.lip > Dieta.carbo && Dieta.lip > Dieta.prot)
+                        nutrienteMaisConsumido = "Lipídios";
+                    else
+                        nutrienteMaisConsumido = "Equilíbrio";
+
+                    Console.WriteLine($"\nNutriente mais consumido: {nutrienteMaisConsumido}");
+
+                    string dietaRecomendada = nutrienteMaisConsumido switch
                     {
-                        Console.WriteLine("Digite entre 1 e 3!");
-                        continue;
-                    }
+                        "Carboidratos" => "Dieta recomendada: reduzir carboidratos refinados e aumentar proteínas magras.",
+                        "Proteínas" => "Dieta recomendada: manter proteínas moderadas e adicionar carboidratos complexos.",
+                        "Lipídios" => "Dieta recomendada: diminuir gorduras saturadas e aumentar gorduras boas.",
+                        _ => "Dieta recomendada: manter equilíbrio entre os macronutrientes."
+                    };
+                        //aqui acaba outra funçao
 
-                    Console.Write("Calorias diárias: ");
-                    Dieta.Calorias = int.Parse(Console.ReadLine());
+
+
+                    Console.WriteLine(dietaRecomendada);
 
                     using (MySqlConnection conexao = new MySqlConnection(conexaoString))
                     {
                         conexao.Open();
 
-                        // Buscar id_paciente
                         string sqlBusca = "select id_paciente from paciente where cpf = @cpf";
                         MySqlCommand cmdBusca = new MySqlCommand(sqlBusca, conexao);
                         cmdBusca.Parameters.AddWithValue("@cpf", cpf);
@@ -101,22 +135,58 @@ namespace proj
 
                         int idPaciente = Convert.ToInt32(result);
 
-                        // Inserir dieta
                         MySqlCommand cmd = new MySqlCommand(Database.sqlInsertDie, conexao);
                         cmd.Parameters.AddWithValue("@idpac", idPaciente);
-                        cmd.Parameters.AddWithValue("@obj",  Dieta.Objetivo);
-                        cmd.Parameters.AddWithValue("@cal", Dieta.Calorias);
+                        cmd.Parameters.AddWithValue("@carb", Dieta.carbo);
+                        cmd.Parameters.AddWithValue("@prot", Dieta.prot);
+                        cmd.Parameters.AddWithValue("@lip", Dieta.lip);
+                        cmd.Parameters.AddWithValue("@cal", Dieta.calorias);
                         cmd.ExecuteNonQuery();
-
-                        Console.WriteLine("\nDieta cadastrada com sucesso!");
                     }
-
+                        //funçao de recomendar uma dieta
+                    Console.WriteLine("\nDieta cadastrada com sucesso!");
                     break;
                 }
+
+
+
                 catch (Exception e)
                 {
                     Console.WriteLine($"Erro: {e.Message}");
                 }
+            }
+        }
+
+        public static void ListarPacientes()
+        {
+            try
+            {
+                Console.WriteLine("LISTA DE PACIENTES CADASTRADOS:");
+                using (MySqlConnection conexao = new MySqlConnection(conexaoString))
+                {
+                    conexao.Open();
+                    string sqlLista = "select id_paciente, nome, idade, cpf, peso, altura from paciente";
+                    MySqlCommand cmd = new MySqlCommand(sqlLista, conexao);
+
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine(
+                                $"ID: {reader["id_paciente"]}, " +
+                                $"Nome: {reader["nome"]}, " +
+                                $"Idade: {reader["idade"]}, " +
+                                $"Peso: {reader["peso"]} kg, " +
+                                $"Altura: {reader["altura"]} m, " +
+                                $"CPF: {reader["cpf"]}"
+                            );
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Erro ao listar pacientes: {e.Message}");
             }
         }
     }
